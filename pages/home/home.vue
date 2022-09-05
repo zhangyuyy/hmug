ca3a627663513f4a6b554d309c051eb584a4333c
zhangyu <2863680540@qq.com>
2022-09-05 11:42:45 +0800

    tabBar 的开发

---
+++ pages/home/home.vue
@@ -0,0 +1,43 @@
+<template>
+  <view>
+    <view class="topSearch">
+      <view class="search_btn">
+        搜索
+      </view>
+    </view>
+  </view>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        
+      };
+    }
+  }
+</script>
+
+<style lang="scss">
+.topSearch {
+  width: 750rpx;
+  height: 70px;
+  background-color: #ea5455
+
+;
+  display: flex;
+  // justify-content: center;
+  overflow: hidden;
+  .search_btn {
+    width: 500rpx;
+    height: 30px;
+    margin:26px 30px;
+    
+    background-color: #fff;
+    border-radius: 10px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+}
+</style>——————
